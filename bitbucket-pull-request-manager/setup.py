8951e781c8b1a330f0ad2cdecc0a339547435365
rahulnehru <rnehru@deloitte.co.uk>
2020-05-24 12:37:22 +0100

    fixup setup.py to work with sdist

--- bitbucket-pull-request-manager/setup.py
+++ bitbucket-pull-request-manager/setup.py
@@ -1,4 +1,9 @@
-setup(
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
     name="bbpr",
     version="0.0.1",
     description="Pull request manager for BitBucket",
